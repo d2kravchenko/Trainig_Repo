c57631b37542bb62e42c9dc6476014fac3071054
d2kravchenko <d2.kravchenko@qa-academy.by>
2020-12-18 13:39:29 +0300

    sync

--- src/test/java/EndPoints.java
+++ src/test/java/EndPoints.java
@@ -1,9 +1,6 @@
-
-
 public class Endpoints {
-
-    public static final String POSTS = "/POSTS";
-    public static final String USERS = "/USERS";
-    public static final String POST_BY_ID = "/POSTS/{id}";
-    public static final String USER_BY_ID = "/USERS/{id}";
+    public static final String POSTS = "/posts";
+    public static final String USERS = "/users";
+    public static final String POST_BY_ID = "/posts/{id}";
+    public static final String USER_BY_ID = "/users/{id}";
 }
